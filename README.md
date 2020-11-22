01be24c517e5035924c001d34182cce12f431768
Daniel Genezini <danielg@grupoht.com.br>
2020-11-22 17:49:02 -0300

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # DIContextAutoLoader.SimpleInjector
+
+[![NuGet version (DIContextAutoLoader.SimpleInjector)](https://img.shields.io/nuget/v/DIContextAutoLoader.SimpleInjector.svg?style=flat-square)](https://www.nuget.org/packages/DIContextAutoLoader.SimpleInjector/)
+
+Extension of [DIContextAutoLoader](https://github.com/dgenezini/DIContextAutoLoader) to use with Ninject.
+
+# Package
+
+```
+    Install-Package DIContextAutoLoader.SimpleInjector
+```
+
+# Usage
+
+```csharp
+private static void RegisterServices(Container container)
+{
+    container.AutoLoadServices(typeof(SomeTypeInAssembly).Assembly);
+
+    ...
+}
+```
---
+++ src/DIAutoLoaderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using SimpleInjector;
+using System.Reflection;
+
+namespace DIContextAutoLoader.SimpleInjector
+{
+    public static class DIAutoLoaderExtensions
+    {
+        public static Container AutoLoadServices(
+            this Container container,
+            params Assembly[] assemblies)
+        {
+            var InstanceInjectionConfigurarions = ServiceInjectionManager
+                .GetServicesInjectionConfigurarions(assemblies);
+
+            foreach (var InstanceInjectionConfigurarion in InstanceInjectionConfigurarions)
+            {
+                if (InstanceInjectionConfigurarion.Lifetime == InjectionLifetime.Transient)
+                {
+                    container
+                        .Register(
+                            InstanceInjectionConfigurarion.ServiceType,
+                            InstanceInjectionConfigurarion.ImplementationType,
+                            Lifestyle.Transient);
+                }
+                else if (InstanceInjectionConfigurarion.Lifetime == InjectionLifetime.Scoped)
+                {
+                    container
+                        .Register(
+                            InstanceInjectionConfigurarion.ServiceType,
+                            InstanceInjectionConfigurarion.ImplementationType,
+                            Lifestyle.Scoped);
+                }
+                else if (InstanceInjectionConfigurarion.Lifetime == InjectionLifetime.Singleton)
+                {
+                    container
+                        .Register(
+                            InstanceInjectionConfigurarion.ServiceType,
+                            InstanceInjectionConfigurarion.ImplementationType,
+                            Lifestyle.Singleton);
+                }
+            }
+
+            return container;
+        }
+    }
+}
